2cd0210011d52853d9e5287d76e0aca931a126a2
Herninda Awwaliya <awwaliyaherninda@gmail.com>
2026-03-16 00:19:45 +0700

    update latihan1_3_classbuku

---
+++ latihan_1/latihan1_3_classbuku.dart
@@ -0,0 +1,17 @@
+class Buku {
+  String? judul;
+  String? pengarang;
+
+  Buku(this.judul, this.pengarang);
+
+  void tampilInfo() {
+    print("Judul Buku: $judul");
+    print("Pengarang: $pengarang");
+  }
+}
+
+void main() {
+  Buku buku1 = Buku("Laskar Pelangi", "Andrea Hirata");
+
+  buku1.tampilInfo();
+}
