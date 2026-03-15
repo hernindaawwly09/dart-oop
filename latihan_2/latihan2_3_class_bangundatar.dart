133091668c5b723521f007f348eac1f517d8ad19
Herninda Awwaliya <awwaliyaherninda@gmail.com>
2026-03-15 19:03:09 +0700

    latihan2_3_class_bangundatar

---
+++ latihan_2/latihan2_3_class_bangundatar.dart
@@ -0,0 +1,36 @@
+class BangunDatar {
+  double hitungLuas() {
+    return 0;
+  }
+}
+
+class Persegi extends BangunDatar {
+  double sisi;
+
+  Persegi(this.sisi);
+
+  @override
+  double hitungLuas() {
+    return sisi * sisi;
+  }
+}
+
+class Segitiga extends BangunDatar {
+  double alas;
+  double tinggi;
+
+  Segitiga(this.alas, this.tinggi);
+
+  @override
+  double hitungLuas() {
+    return 0.5 * alas * tinggi;
+  }
+}
+
+void main() {
+  Persegi persegi = Persegi(4);
+  Segitiga segitiga = Segitiga(6, 3);
+
+  print("Luas Persegi: ${persegi.hitungLuas()}");
+  print("Luas Segitiga: ${segitiga.hitungLuas()}");
+}
