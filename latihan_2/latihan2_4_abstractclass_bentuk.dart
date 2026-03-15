b0ae3c9791f9a57d3a2846ad73d5914ac71229f3
Herninda Awwaliya <awwaliyaherninda@gmail.com>
2026-03-15 19:03:27 +0700

    latihan2_4_abstractclass_bentuk

---
+++ latihan_2/latihan2_4_abstractclass_bentuk.dart
@@ -0,0 +1,20 @@
+abstract class Bentuk {
+  double hitungLuas();
+}
+
+class Lingkaran extends Bentuk {
+  double jariJari;
+
+  Lingkaran(this.jariJari);
+
+  @override
+  double hitungLuas() {
+    return 3.14 * jariJari * jariJari;
+  }
+}
+
+void main() {
+  Lingkaran lingkaran = Lingkaran(7);
+
+  print("Luas Lingkaran: ${lingkaran.hitungLuas()}");
+}
