083bfd10cea0b25a06a19893200d2a1812263a91
Herninda Awwaliya <awwaliyaherninda@gmail.com>
2026-03-16 08:57:43 +0700

    update polymorphism.dart

---
+++ lib_materi/5_polymorphism/polymorphism.dart
@@ -0,0 +1,12 @@
+class ParentClass{
+    void functionName(){
+        //isi function di parent
+    }
+}
+
+class ChildClass extends ParentClass{
+    @override
+    void functionName(){
+        //isi function di child yang berbeda dari parent
+    }
+}
